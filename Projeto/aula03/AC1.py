85e9ea3e0249fc0a201e0a394209563cdb04cc96
VINICIUS LEME PIERINA <150138@facens.br>
2020-03-12 22:24:23 -0300

    Starting the project

--- Projeto/aula03/AC1.py
+++ Projeto/aula03/AC1.py
@@ -4,23 +4,15 @@ import time
 
 os.chdir("Imgs")
 while True :
+
     for file in glob.glob("*.jpg"):
         img = cv2.imread(file)
         cv2.imshow('Img',img)
-        cv2.waitKey(5)
+        cv2.waitKey(5000)
         print(file)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# img = cv2.imread('Imgs/lena.jpg')
-
-# cv2.imshow('Img',img)
-
-# print('Shape: ', img.shape)
-# print('Size: ' , img.size)
-# print('Type: ' , img.dtype)
-
-# cv2.waitKey()
-
-# cv2.destroyAllWindows()
+        
+    if 0xFF == ord('q') | 0xFF == ord('Q'): 
+        print("batata")
+        cv2.destroyAllWindows()
+        break
 
